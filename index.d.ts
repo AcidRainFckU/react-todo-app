9ba2b54207be023d4ef5bee299ad4be9c7aded39
Илья Матвеев <matweev.ilja@yandex.ru>
2022-02-05 18:41:41 +0300

    Инициализация

---
+++ index.d.ts
@@ -0,0 +1,10 @@
+declare module '*.jpg'
+declare module "*.png"
+declare module "*.svg" {
+    const value: any;
+    export = value;
+ }
+declare module "*.module.css" {
+    const value: any;
+    export = value;
+ }
---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import './styles.scss'
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>FUCK MY ASS</h1>
+    </div>
+  )
+}
