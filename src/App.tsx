eefdd8fa2c60c0e51ca06e7fbf3865179db6b7da
Илья Матвеев <matweev.ilja@yandex.ru>
2022-02-06 16:39:58 +0300

    Сделал 1 урок

--- src/App.tsx
+++
@@ -1,9 +0,0 @@
-import './styles.scss'
-
-export default function App() {
-  return (
-    <div className="App">
-      <h1>FUCK MY ASS</h1>
-    </div>
-  )
-}
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './App'
+import App from './tutorial-1/App'
+import './styles.scss'
 
 ReactDOM.render(
   <React.StrictMode>
